2212b78d67aff0a94b938b94ec535eee4f5d2802
GreyGreyWolf <57860055+GreyGreyWolf@users.noreply.github.com>
2020-02-21 13:52:23 +0300

    q

--- scripts/gendiff.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
- 
-parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-
-parser.print_help()
