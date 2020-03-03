fb75daf0ed71ed62694c055b84814d60b81085b8
GreyGreyWolf <Wolfdieattheend@yandex.ru>
2020-03-03 12:59:41 +0300

    Step4

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ lint:
 	@poetry run flake8 gen_diff
 	
 test:
-	poetry run pytest --cov --cov-report xml tests/
+	python3 -m poetry run pytest --cov --cov-report xml tests/
--- tests/tests_diff_plain.py
+++ tests/tests_diff_plain.py
@@ -1,6 +1,12 @@
 import pytest
+from gen_diff import engine
+
+def reader(file):
+    with open(file, 'r') as input_file:
+        answer = input_file.read()
+    return answer
 
 
-считать файд
-
-проверить равен ли ответ в нем тому , что выдает моя функция
+def test_answer():
+    assert reader('./tests/fixtures/result.txt') == engine.engine_diff('./tests/fixtures/before.json',
+                                                                       './tests/fitures/after.json')
