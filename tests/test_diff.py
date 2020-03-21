f83df8b9657fe5b3e67b18fab48590fbbe27f529
GreyGreyWolf <WolfDieAtTheEnd@yandex.ru>
2020-03-21 22:02:59 +0300

    Fix

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -3,7 +3,6 @@ import yaml
 import json
 from gen_diff import engine
 from gen_diff.parsers import parser
-# from gen_diff.format. import format
 from gen_diff import format
 
 
@@ -17,34 +16,27 @@ def test_answer():
     assert read(
         './tests/fixtures/result.txt') == format.default(
                                         engine.get_diff(
-                                        './tests/fixtures/before.json',
-                                        './tests/fixtures/after.json'
-                                        )
-        )
+                                         './tests/fixtures/before.json',
+                                         './tests/fixtures/after.json'
+                                         ))
     assert yaml.safe_load(open(
         './tests/fixtures/before.yml')) == parser(
             './tests/fixtures/before.yml')
     assert isinstance(format.default(
         engine.get_diff(
-        './tests/fixtures/before.yml',
-        './tests/fixtures/after.yml'
-        )
-    ), str)
+         './tests/fixtures/before.yml',
+         './tests/fixtures/after.yml'
+         )), str)
     assert read(
         './tests/fixtures/recursion_result.txt') == format.default(
         engine.get_diff(
-        './tests/fixtures/complex_before.json',
-        './tests/fixtures/complex_after.json'
-        )
-    )
-    assert read('./tests/fixtures/text_result.txt') == format.plain(engine.get_diff(
-        './tests/fixtures/complex_before.json',
-         './tests/fixtures/complex_after.json'
-         )
-    )
+         './tests/fixtures/complex_before.json',
+         './tests/fixtures/complex_after.json'))
+    assert read('./tests/fixtures/text_result.txt') == format.plain(
+        engine.get_diff(
+         './tests/fixtures/complex_before.json',
+         './tests/fixtures/complex_after.json'))
     assert read('tests/fixtures/json_result.json') == format.json(
         engine.get_diff(
-        './tests/fixtures/before.json',
-        './tests/fixtures/after.json'
-        )
-    )
+         './tests/fixtures/before.json',
+         './tests/fixtures/after.json'))
